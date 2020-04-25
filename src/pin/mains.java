b7f6fcf2036a4c0dc0cb4b37f4e99e03a1cf34b0
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-04-24 22:37:41 -0600

    Update

--- src/pin/mains.java
+++ src/pin/mains.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pin;
 
-/**
- *
- * @author ccten
- */
 public class mains {
-    
+    static void main(String[] args){
+
+	System.out.println("Hello World");
+	
+}
 }
