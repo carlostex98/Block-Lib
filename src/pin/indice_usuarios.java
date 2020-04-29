1873e24a78490ce9c8b405e76f0d670874635de5
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-04-29 17:37:39 -0600

    Update

---
+++ src/pin/indice_usuarios.java
@@ -0,0 +1,9 @@
+package pin;
+
+public class indice_usuarios {
+    //aca se almacenaran los indices de la tabla hash
+    int indice;
+    indice_usuarios siguiente;
+    usuarios primero_usr;
+    
+}
---
+++ src/pin/usuarios.java
@@ -0,0 +1,9 @@
+package pin;
+public class usuarios {
+    //lista de los usuarios, de las colisiones de la tabla hash
+    int carnet;
+    String nombre;
+    String apellido;
+    String carrera;
+    String psw;
+}
