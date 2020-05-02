34ccf6193a26f0bd12d4940d568ecd9f8bc8b16e
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-05-01 22:25:29 -0600

    Update

--- src/pin/Btree.java
+++ src/pin/Btree.java
@@ -1,6 +1,28 @@
-
 package pin;
 
 public class Btree {
-    
+
+    static int orden;
+    BNode root;
+
+    public Btree(int orden) {
+        this.orden = orden;
+        root = new BNode(orden, null);
+
+    }
+
+    public BNode busca(BNode root, int key) {
+        int i = 0;
+        while (i < root.cuenta && key > root.isbn[i]){
+            i++;
+        }
+        if (i <= root.cuenta&& key == root.isbn[i]){
+            return root;
+        }
+        if (root.hoja){
+            return null;
+        }else{
+            return busca(root.getChild(i), key);
+        }
+    }
 }
