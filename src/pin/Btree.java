a53f955c97f80d14ef32b631c2cbbff3f4b5223f
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-05-02 21:52:54 -0600

    Nice Update

--- src/pin/Btree.java
+++ src/pin/Btree.java
@@ -26,5 +26,43 @@ public class Btree {
         }
     }
     
+    public void splitea(BNode x, int i, BNode y){
+		BNode z = new BNode(orden,null);//gotta have extra node if we are
+	                					//to split.
+
+		z.hoja = y.hoja;//set boolean to same as y
+
+		z.cuenta = orden - 1;//this is updated size
+
+		for(int j = 0; j < orden - 1; j++)
+		{
+			z.isbn[j] = y.isbn[j+orden]; //copy end of y into front of z
+
+		}
+		if(!y.hoja)//if not leaf we have to reassign child nodes.
+		{
+                    for(int k = 0; k < orden; k++){
+                            z.hijo[k] = y.hijo[k+orden];
+                    }
+		}
+		y.cuenta = orden - 1; 
+
+		for(int j = x.cuenta ; j> i ; j--){
+                    x.hijo[j+1] = x.hijo[j];
+		}
+		x.hijo[i+1] = z;
+
+		for(int jp = x.cuenta; jp> i; jp--){
+			x.isbn[jp + 1] = x.isbn[jp];
+		}
+		x.isbn[i] = y.isbn[orden-1];
+		y.isbn[orden-1] = 0;
+
+		for(int j = 0; j < orden - 1; j++){
+			y.isbn[j + orden] = 0;
+		}
+		x.cuenta ++;
+	}
+
     
 }
