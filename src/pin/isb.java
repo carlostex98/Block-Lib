4a944494a703e1b040d995765881d5b76a165cf3
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-05-04 16:02:20 -0600

    Update

--- src/pin/isb.java
+++ src/pin/isb.java
@@ -1,4 +1,14 @@
 package pin;
 public class isb {
     int isbn;
+    String titulo;
+    String autor;
+    String editorial;
+    String anio;
+    String edicion;
+    String categoria;
+    String idioma;
+    int carnet;
+    isb sig;
+    isb ant;
 }
---
+++ src/pin/lisb.java
@@ -0,0 +1,19 @@
+package pin;
+
+public class lisb {
+
+    isb primero;
+    isb ultimo;
+
+    public lisb() {
+        primero = ultimo = null;
+    }
+    
+    public void insert_l(int isbn, String titulo, String autor, String edit, int anio, String edidcion, String cat, String idiom){
+        if(primero==null){
+            
+        }else{
+            
+        }
+    }
+}
